d89798ec888c515147e9a63f506e4ebb4012da10
Thomas Cassou <thomas.cassou@booking.com>
2017-10-25 22:54:15 +0200

    bump version and remove license details

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@
 from distutils.core import setup
 
 
+version = '1.0.1'
+
 setup(
     name='causal_impact',
     packages=['causal_impact'],
-    version='1.0.0',
+    version=version,
     description='Python package for causal inference using Bayesian structural time-series models.',
     url='https://github.com/tcassou/causal_impact',
-    download_url='https://github.com/tcassou/causal_impact/archive/1.0.0.tar.gz',
+    download_url='https://github.com/tcassou/causal_impact/archive/{}.tar.gz'.format(version),
     keywords=['bayesian', 'structural', 'time-series', 'causal', 'impact', 'python', 'inference'],
-    license='MIT',
     classifiers=[
         'Programming Language :: Python',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Visualization',
