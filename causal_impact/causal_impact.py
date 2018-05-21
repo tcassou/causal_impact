e3378db0377ad2500178c1c213b29c99936a86c5
Thomas Cassou <thomas.cassou@booking.com>
2018-05-21 15:54:35 +0200

    remove unused import

--- causal_impact/causal_impact.py
+++ causal_impact/causal_impact.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-# from __future__ import unicode_literals
 
 import matplotlib.pyplot as plt
 import numpy as np
